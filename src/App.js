863fcc9d42e2459ff1e9386a679553b33c7d2280
oddtomas <oddthomas@csu.fullerton.edu>
2021-07-27 12:15:41 -0700

    got stuff on the board

--- src/App.js
+++ src/App.js
@@ -6,15 +6,53 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [" ", " "," "," "," "," "," "," "," "],
+      player: "X",
+      player2: "O",
+      value: null
     }
   }
+
+handleGamePlay = (index) => {
+let {squares} = this.state
+this.setState({squares: squares}) 
+
+let {player1} = this.state
+this.setState({player1: player1}) 
+squares[index] = player1
+
+let {player2} = this.state
+this.setState({player2: player2}) 
+squares[index] = player2
+
+
+// if (!squares[index]){
+//   squares[index] = player1
+//   player1 === "X" ? player2 = "O" : player1 = "X"
+//   this.setState({squares: "X", player: player})
+
+// }
+
+
+//this.state = { player:1 , winner: false}
+//return this.state.player === 1 ? 2 : 1;
+
+}
 
   render(){
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div id="gameboard">
+        {this.state.squares.map((value, index) => {
+          return <Square value={value}
+           key={index} 
+           index={index}
+           handleGamePlay={this.handleGamePlay}
+           />
+        } )}
+    
+        </div>
       </>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+
+
+
+handleClick = () => {
+  this.props.handleGamePlay(this.props.index)
+}
+
+
+
   render(){
     return(
       <>
-        <div className="square"></div>
+        <div className="square" onClick= {this.handleClick}>
+          {this.props.value}
+        </div>
       </>
     )
   }
 }
 export default Square
+
+
+{/* <button
+className="square"
+onClick={() => this.setState({value: 'X'})}
+>
+{this.state.value}
+</button> */}
