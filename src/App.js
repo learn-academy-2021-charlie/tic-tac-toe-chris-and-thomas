673b51a0708f7472cd2858ae6eb9c28b268f8afc
oddtomas <oddthomas@csu.fullerton.edu>
2021-07-27 16:47:18 -0700

    click disabler working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Square from './components/Square'
 import './App.css'
 
 class App extends Component {
@@ -7,14 +6,20 @@ class App extends Component {
     super(props)
     this.state = {
       squares: ["", "", "", "", "", "", "", "", ""],
-      playerTurn: true,
+      playerTurn: "X",
       count: 9
+      
     }
   }
 
   handleClick = (index) => {
     let playerTurn = this.state.playerTurn
     let squares = this.state.squares
+
+if(squares[index] !== ""){
+  return
+}
+
     this.setState({ count: this.state.count - 1 })
     squares[index] = playerTurn;
     let win = [
@@ -32,7 +37,7 @@ class App extends Component {
       let p1 = winCombo[0]
       let p2 = winCombo[1]
       let p3 = winCombo[2]
-      if (squares[p1] != "" && squares[p1] == squares[p2] && squares[p2] == squares[p3] && squares[p3] == squares[p1]) {
+      if (squares[p1] !== "" && squares[p1] === squares[p2] && squares[p2] === squares[p3] && squares[p3] === squares[p1]) {
         alert(`Winner! player ${playerTurn}`)
         return this.refreshPage()
       } else if (this.state.count === 1) {
@@ -42,19 +47,25 @@ class App extends Component {
 
     }
 
-    if (playerTurn === true && squares[index] !== '⭕️') {
-      squares[index] = '❌'
-      this.setState({
-        squares: squares,
-        playerTurn: false
-      })
-    } else if (playerTurn === false && squares[index] !== '❌') {
-      squares[index] = '⭕️'
-      this.setState({
-        squares: squares,
-        playerTurn: true
-      })
-    }
+    // if (playerTurn === true && squares[index] !== '⭕️') {
+    //   squares[index] = '❌'
+    //   this.setState({
+    //     squares: squares,
+    //     playerTurn: false
+    //   })
+    // } else if (playerTurn === false && squares[index] !== '❌') {
+    //   squares[index] = '⭕️'
+    //   this.setState({
+    //     squares: squares,
+    //     playerTurn: true
+    //   })
+    // }
+
+    playerTurn = (playerTurn === "X") ? "O" : "X";
+    this.setState({
+      playerTurn: playerTurn,
+      squares: squares
+    })
   }
   refreshPage = () => {
     window.location.reload();
@@ -66,7 +77,7 @@ class App extends Component {
         <h1>Tic Tac Toe</h1>
         <p id="counter">Count: {this.state.count}</p>
         <p>{this.state.playerTurn ? "player 1" : "player2"}</p>
-        <button>💣</button>
+        {/* <button>💣</button> */}
         <div id="gameboard">
 
           {this.state.squares.map((square, index) => {
@@ -74,6 +85,9 @@ class App extends Component {
           })}
         </div>
         <button onClick={this.refreshPage}>Click to restart</button>
+
+
+        <button>Marker Selector</button>
 
       </>
     )
